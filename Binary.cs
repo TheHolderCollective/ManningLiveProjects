86faf1529aa476f4aea91ba5737f9bc6d3411e60
R Holder <101442317+TheHolderCollective@users.noreply.github.com>
2023-05-28 19:51:36 -0700

    Add files via upload

---
+++ Binary.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace binary_node1
+{
+    internal class BinaryNode<T>
+    {
+        internal T Value { get; set; }
+        internal BinaryNode<T> LeftChild;
+        internal BinaryNode<T> RightChild;
+
+        internal BinaryNode(T value)
+        {
+            
+            Value = value;
+            LeftChild = null;
+            RightChild = null;
+        }
+
+        internal void AddLeft(BinaryNode<T> leftvalue)
+        {
+            LeftChild = leftvalue;
+        }
+
+        internal void AddRight(BinaryNode<T> rightvalue)
+        {
+            RightChild = rightvalue;
+        }
+
+        public override string ToString()
+        {
+            string result;
+
+            result = string.Format("{0}:" ,Value);
+
+            if (LeftChild != null)
+            {
+                result += " " + LeftChild.Value;
+            }
+            else
+            {
+                result += " null";
+
+            }
+
+            if (RightChild != null)
+            {
+                result += " " + RightChild.Value;
+            }
+            else
+            {
+                result += " null";
+            }
+
+            return result;
+
+        }
+        
+    }   
+    class Binary
+    {
+        static void Main(string[] args)
+        {
+            BinaryNode<string> BinaryTreeRoot = new BinaryNode<string>("Root");
+
+            // child nodes of root node
+            BinaryNode<string> L1_LeftNode = new BinaryNode<string>("A");
+            BinaryNode<string> L1_RightNode = new BinaryNode<string>("B");
+
+            // left and right nodes of left node of root node
+            BinaryNode<string> L2_L_LeftNode = new BinaryNode<string>("C");
+            BinaryNode<string> L2_L_RightNode = new BinaryNode<string>("D");
+
+            // right node of right child node of root node
+            BinaryNode<string> L2_R_RightNode = new BinaryNode<string>("E");
+
+            // left node of right child node of left child node of root node
+            BinaryNode<string> L3_R_LeftNode = new BinaryNode<string>("F");
+
+            //add children to root node
+            BinaryTreeRoot.AddLeft(L1_LeftNode);
+            BinaryTreeRoot.AddRight(L1_RightNode);
+
+            // add children to left node of left node of root node
+            L1_LeftNode.AddLeft(L2_L_LeftNode);
+            L1_LeftNode.AddRight(L2_L_RightNode);
+
+            // add children to right node of right node of root node
+            L1_RightNode.AddRight(L2_R_RightNode);
+
+            // add children to right node of right child node of root node
+            L2_R_RightNode.AddRight(L3_R_LeftNode);
+
+
+            Console.WriteLine(BinaryTreeRoot);
+            Console.WriteLine(L1_LeftNode);
+            Console.WriteLine(L1_RightNode);
+            Console.WriteLine(L2_L_LeftNode);
+            Console.WriteLine(L2_L_RightNode);
+            Console.WriteLine(L2_R_RightNode);
+            Console.WriteLine(L3_R_LeftNode);
+        }
+    }
+}
---
+++ NaryNode.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace naray_node1
+{
+    internal class NaryNode<T>
+    {
+        internal T Value { get; set; }
+        internal List<NaryNode<T>> Children;
+
+        internal  NaryNode(T value)
+        {
+            Value = value;
+            Children = new List<NaryNode<T>>();
+        }
+
+        internal void AddChild(NaryNode<T> childnode) 
+        {
+            Children.Add(childnode);
+        }
+
+        public override string ToString()
+        {
+            string result = string.Format("{0}: ", Value);
+
+            foreach (NaryNode<T> node in Children)
+            {
+               result += string.Format("{0} ", node.Value);
+            }
+
+            return result;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            NaryNode<string> NaryRoot = new NaryNode<string>("Root");
+
+            // children of root node
+            NaryNode<string> L1_Child1 = new NaryNode<string>("A");
+            NaryNode<string> L1_Child2 = new NaryNode<string>("B");
+            NaryNode<string> L1_Child3 = new NaryNode<string>("C");
+
+            // children of child1 of root node
+            NaryNode<string> L2_Child1_1 = new NaryNode<string>("D");
+            NaryNode<string> L2_Child1_2 = new NaryNode<string>("E");
+
+            // children of child3 of root node
+            NaryNode<string> L2_Child3_1 = new NaryNode<string>("F");
+
+            // children of child1 of child 1 of root node
+            NaryNode<string> L3_Child1_1_1 = new NaryNode<string>("G");
+
+            //children of child1 of child 3 of root node
+            NaryNode<string> L3_Child3_1_1 = new NaryNode<string>("H");
+            NaryNode<string> L3_Child3_1_2 = new NaryNode<string>("I");
+
+            // add children to root node
+            NaryRoot.AddChild(L1_Child1);
+            NaryRoot.AddChild(L1_Child2);
+            NaryRoot.AddChild(L1_Child3);
+
+            // add children to child1 of root node
+            L1_Child1.AddChild(L2_Child1_1);
+            L1_Child1.AddChild(L2_Child1_2);
+
+            // add children to child3 of root node
+            L1_Child3.AddChild(L2_Child3_1);
+
+            // add children to child1 of child 1 of root node
+            L2_Child1_1.AddChild(L3_Child1_1_1);
+
+            // add children to child1 of child 3 of root node
+            L2_Child3_1.AddChild(L3_Child3_1_1);
+            L2_Child3_1.AddChild(L3_Child3_1_2);
+
+
+
+            Console.WriteLine(NaryRoot);
+            Console.WriteLine(L1_Child1);
+            Console.WriteLine(L1_Child2);
+            Console.WriteLine(L1_Child3);
+            Console.WriteLine(L2_Child1_1);
+            Console.WriteLine(L2_Child1_2);
+            Console.WriteLine(L2_Child3_1);
+            Console.WriteLine(L3_Child1_1_1);
+            Console.WriteLine(L3_Child3_1_1);
+            Console.WriteLine(L3_Child3_1_2);
+
+        }
+    }
+}
