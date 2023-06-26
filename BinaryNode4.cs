a2aa0a5c4cba1c486357c65b167364273ff4e6d4
R Holder <101442317+TheHolderCollective@users.noreply.github.com>
2023-06-25 19:13:34 -0700

    Add files via upload

---
+++ BinaryNode4.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace binary_node4
+{
+
+    internal class BinaryNode<T>
+    {
+        internal T Value { get; set; }
+        internal BinaryNode<T> LeftChild;
+        internal BinaryNode<T> RightChild;
+        
+
+        internal BinaryNode(T value)
+        {
+
+            Value = value;
+            LeftChild = null;
+            RightChild = null;
+            //QueueofNodes = new Queue<BinaryNode<T>>;
+        }
+
+        internal void AddLeft(BinaryNode<T> leftvalue)
+        {
+            LeftChild = leftvalue;
+        }
+
+        internal void AddRight(BinaryNode<T> rightvalue)
+        {
+            RightChild = rightvalue;
+        }
+        public override string ToString()
+        {
+            return ToString("");
+        }
+
+        public string ToString(string spaces)
+        {
+            string result = spaces + Value + ":\n";
+
+            if ((LeftChild != null) || (RightChild != null))
+            {
+                if (LeftChild != null)
+                {
+                    result += LeftChild.ToString(spaces + "  ");
+                }
+                else
+                {
+                    result += "  " + spaces + "None" + ":\n";
+                }
+
+                if (RightChild != null)
+                {
+                    result += RightChild.ToString(spaces + "  ");
+                }
+                else
+                {
+                    result += "  " + spaces + "None" + ":\n";
+                }
+            }
+            return result;
+
+        }
+
+        public BinaryNode<T> FindNode(T value)
+        {
+            if (Value.Equals(value))
+            {
+                return this;
+            }
+            else if ((LeftChild != null) && (LeftChild.FindNode(value) != null))
+            {
+                return LeftChild;
+            }
+            else if ((RightChild != null) && (RightChild.FindNode(value) != null))
+            {
+                return RightChild;
+            }
+            return null;
+        }
+
+
+        public string TraversePreOrder()
+        {
+
+            //this.result = this.Value + " ";
+
+            string result = this.Value + " ";
+
+            if (LeftChild != null)
+            {
+                result += LeftChild.TraversePreOrder();
+            }
+            if (RightChild != null)
+            {
+                result += RightChild.TraversePreOrder();
+            }
+            return result;
+        }
+
+        public string TraverseInOrder()
+        {
+            string result = "";
+
+            if (LeftChild != null)
+            {
+                result += LeftChild.TraverseInOrder();
+            }
+
+            result += this.Value + " ";
+
+            if (RightChild != null)
+            {
+                result += RightChild.TraverseInOrder();
+            }
+            return result; 
+
+        }
+
+        public string TraversePostOrder()
+        {
+            string result = "";
+
+            if (LeftChild != null)
+            {
+                result += LeftChild.TraversePostOrder();
+            }
+
+
+            if (RightChild != null)
+            {
+                result += RightChild.TraversePostOrder();
+            }
+
+            result += this.Value + " ";
+
+            return result;
+
+        }
+
+        public string TraverseBreadthFirst()
+        {
+            string result = "";
+            BinaryNode<T> node;
+            Queue<BinaryNode<T>> queue = new Queue<BinaryNode<T>>();
+
+            queue.Enqueue(this);
+
+            while(queue.Count != 0)
+            {
+                node = queue.Dequeue();
+                result += node.Value + " ";
+
+                if (node.LeftChild != null)
+                {
+                    queue.Enqueue(node.LeftChild);
+                }
+                if (node.RightChild != null)
+                {
+                    queue.Enqueue(node.RightChild);
+                }
+            }
+
+            return result;
+
+        }
+
+
+
+    }
+}
---
+++ NaryNode4.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nary_node4
+{
+    internal class NaryNode<T>
+    {
+        internal T Value { get; set; }
+        internal List<NaryNode<T>> Children;
+
+        internal NaryNode(T value)
+        {
+            Value = value;
+            Children = new List<NaryNode<T>>();
+        }
+
+        internal void AddChild(NaryNode<T> childnode)
+        {
+            Children.Add(childnode);
+        }
+
+        public override string ToString()
+        {
+            return ToString("");
+        }
+
+        public string ToString(string spaces)
+        {
+            string result = spaces + Value + ":\n";
+
+            foreach (NaryNode<T> node in Children)
+            {
+                if (node != null)
+                {
+                    result += node.ToString(spaces + "  ");
+                }
+            }
+
+            return result;
+        }
+
+        public NaryNode<T> FindNode(T value)
+        {
+            if (Value.Equals(value))
+            {
+                return this;
+            }
+            else
+            {
+                foreach (NaryNode<T> node in Children)
+                {
+                    if (node.FindNode(value) != null)
+                    {
+                        return node;
+                    }
+
+                }
+            }
+            return null;
+        }
+
+        public string TraversePreOrder()
+        {
+            string result = this.Value + " ";
+
+            foreach (NaryNode<T> node in Children)
+            {
+                if (node != null)
+                {
+                    result += node.TraversePreOrder();
+                }
+            }
+            return result;
+        }
+
+        public string TraversePostOrder()
+        {
+            string result = "";
+
+            foreach (NaryNode<T> node in Children)
+            {
+                if (node != null)
+                {
+                    result += node.TraversePostOrder() + " ";
+                }
+            }
+
+            result += this.Value;
+
+            return result;
+
+        }
+
+        public string TraverseBreadthFirst()
+        {
+            string result = "";
+            NaryNode<T> node;
+            Queue<NaryNode<T>> queue = new Queue<NaryNode<T>>();
+
+            queue.Enqueue(this);
+
+            while (queue.Count != 0)
+            {
+                node = queue.Dequeue();
+                result += node.Value + " ";
+
+                foreach (NaryNode<T> nd in node.Children)
+                {
+                    queue.Enqueue(nd);
+                }
+              
+            }
+
+            return result;
+        }
+    }
+}
